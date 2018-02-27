cd6433d3304eb87282b1b33e1665aafa6f2e2752
William Antony <william@pathadan.com>
2018-02-27 11:47:16 -0600

    implemented RAM

--- project/ls8/ram.js
+++ project/ls8/ram.js
@@ -11,8 +11,8 @@ class RAM {
      * Write (store) MDR value at address MAR
      */
     write(MAR, MDR) {
-        // !!! IMPLEMENT ME
         // write the value in the MDR to the address MAR
+        this.mem[MAR] = MDR;
     }
 
     /**
@@ -21,8 +21,8 @@ class RAM {
      * @returns MDR
      */
     read(MAR) {
-        // !!! IMPLEMENT ME
         // Read the value in address MAR and return it
+        return this.mem[MAR];
     }
 }
 
