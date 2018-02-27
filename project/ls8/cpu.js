8869d726e34d1f129409c290d745656b7bf42507
William Antony <william@pathadan.com>
2018-02-27 11:48:47 -0600

    implemented CPU

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -6,11 +6,15 @@ const fs = require('fs');
 
 // Instructions
 
-const HLT  = 0b00000001; // Halt CPU
-// !!! IMPLEMENT ME
+const HLT = 0b00000001; // Halt CPU
 // LDI
+const LDI = 0b10011001;
 // MUL
+const MUL = 0b10101010;
+// Add
+const ADD = 0b10101000;
 // PRN
+const PRN = 0b01000011;
 
 /**
  * Class for simulating a simple Computer (CPU & memory)
@@ -29,23 +33,28 @@ class CPU {
         this.reg.PC = 0; // Program Counter
         this.reg.IR = 0; // Instruction Register
 
-		this.setupBranchTable();
+        this.setupBranchTable();
     }
-	
-	/**
-	 * Sets up the branch table
-	 */
-	setupBranchTable() {
-		let bt = {};
+    
+    /**
+     * Sets up the branch table
+     */
+    setupBranchTable() {
+        let bt = {};
 
         bt[HLT] = this.HLT;
         // !!! IMPLEMENT ME
         // LDI
+        bt[LDI] = this.LDI;
         // MUL
+        bt[MUL] = this.MUL;
+        // ADD
+        bt[ADD] = this.ADD;
         // PRN
+        bt[PRN] = this.PRN;
 
-		this.branchTable = bt;
-	}
+        this.branchTable = bt;
+    }
 
     /**
      * Store value in memory address, useful for program loading
@@ -79,8 +88,11 @@ class CPU {
      */
     alu(op, regA, regB) {
         switch (op) {
+            case 'ADD':
+                this.reg[regA] = this.reg[regA] + this.reg[regB];
+                break;
             case 'MUL':
-                // !!! IMPLEMENT ME
+                this.reg[regA] = this.reg[regA] * this.reg[regB];
                 break;
         }
     }
@@ -91,6 +103,7 @@ class CPU {
     tick() {
         // Load the instruction register (IR) from the current PC
         // !!! IMPLEMENT ME
+        this.reg.IR = this.ram.read(this.reg.PC);
 
         // Debugging output
         //console.log(`${this.reg.PC}: ${this.reg.IR.toString(2)}`);
@@ -98,11 +111,19 @@ class CPU {
         // Based on the value in the Instruction Register, locate the
         // appropriate hander in the branchTable
         // !!! IMPLEMENT ME
-        // let handler = ...
+        const handler = this.branchTable[this.reg.IR];
 
         // Check that the handler is defined, halt if not (invalid
         // instruction)
+        if (handler === undefined) {
+            console.log(`Instruction ${ this.reg.IR } is invalid`);
+            this.stopClock();
+            return;
+        }
+
         // !!! IMPLEMENT ME
+        const operandA = this.ram.read(this.reg.PC + 1);
+        const operandB = this.ram.read(this.reg.PC + 2);
 
         // We need to use call() so we can set the "this" value inside
         // the handler (otherwise it will be undefined in the handler)
@@ -110,6 +131,8 @@ class CPU {
 
         // Increment the PC register to go to the next instruction
         // !!! IMPLEMENT ME
+        const num = (this.reg.IR >> 6) & 0b00000011;
+        this.reg.PC += (num + 1);
     }
 
     // INSTRUCTION HANDLER CODE:
@@ -126,6 +149,7 @@ class CPU {
      */
     LDI(reg, value) {
         // !!! IMPLEMENT ME
+        this.reg[reg] = value;
     }
 
     /**
@@ -134,6 +158,16 @@ class CPU {
     MUL(regA, regB) {
         // !!! IMPLEMENT ME
         // Call the ALU
+        this.alu('MUL', regA, regB);
+    }
+
+    /**
+     * ADD R,R
+     */
+    ADD(regA, regB) {
+        // !!! IMPLEMENT ME
+        // Call the ALU
+        this.alu('ADD', regA, regB);
     }
 
     /**
@@ -141,6 +175,9 @@ class CPU {
      */
     PRN(regA) {
         // !!! IMPLEMENT ME
+        const value = this.reg[regA];
+        // Print
+        console.log(value);
     }
 }
 
