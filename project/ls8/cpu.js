8406d27d27d35cf8c4f702dc61ad591b26249041
William Antony <william@pathadan.com>
2018-03-02 11:38:13 -0600

    Cleaned up after adding MUL

--- project/ls8/cpu.js
+++ project/ls8/cpu.js
@@ -6,15 +6,13 @@ const fs = require('fs');
 
 // Instructions
 
-const HLT = 0b00000001; // Halt CPU
-// LDI
+const HLT = 0b00000001;
+
 const LDI = 0b10011001;
-// MUL
-const MUL = 0b10101010;
-// Add
-const ADD = 0b10101000;
-// PRN
 const PRN = 0b01000011;
+
+const ADD = 0b10101000;
+const MUL = 0b10101010;
 
 /**
  * Class for simulating a simple Computer (CPU & memory)
@@ -32,6 +30,11 @@ class CPU {
         // Special-purpose registers
         this.reg.PC = 0; // Program Counter
         this.reg.IR = 0; // Instruction Register
+        this.reg.FL = 0; // Flags
+
+        this.reg[IM] = 0;
+        this.reg[IS] = 0;
+        this.reg[SP] = 0xF4 // Stack Pointer
 
         this.setupBranchTable();
     }
@@ -43,15 +46,12 @@ class CPU {
         let bt = {};
 
         bt[HLT] = this.HLT;
-        // !!! IMPLEMENT ME
-        // LDI
+        
         bt[LDI] = this.LDI;
-        // MUL
-        bt[MUL] = this.MUL;
-        // ADD
-        bt[ADD] = this.ADD;
-        // PRN
         bt[PRN] = this.PRN;
+
+        bt[ADD] = this.ADD;
+        bt[MUL] = this.MUL;
 
         this.branchTable = bt;
     }
@@ -80,7 +80,6 @@ class CPU {
     stopClock() {
         clearInterval(this.clock);
     }
-
     /**
      * ALU functionality
      * 
@@ -89,10 +88,10 @@ class CPU {
     alu(op, regA, regB) {
         switch (op) {
             case 'ADD':
-                this.reg[regA] = this.reg[regA] + this.reg[regB];
+                this.reg[regA] = (this.reg[regA] + this.reg[regB]) & 255;
                 break;
             case 'MUL':
-                this.reg[regA] = this.reg[regA] * this.reg[regB];
+                this.reg[regA] = (this.reg[regA] * this.reg[regB]) & 255;
                 break;
         }
     }
@@ -102,7 +101,6 @@ class CPU {
      */
     tick() {
         // Load the instruction register (IR) from the current PC
-        // !!! IMPLEMENT ME
         this.reg.IR = this.ram.read(this.reg.PC);
 
         // Debugging output
@@ -110,7 +108,6 @@ class CPU {
 
         // Based on the value in the Instruction Register, locate the
         // appropriate hander in the branchTable
-        // !!! IMPLEMENT ME
         const handler = this.branchTable[this.reg.IR];
 
         // Check that the handler is defined, halt if not (invalid
@@ -121,64 +118,45 @@ class CPU {
             return;
         }
 
-        // !!! IMPLEMENT ME
         const operandA = this.ram.read(this.reg.PC + 1);
         const operandB = this.ram.read(this.reg.PC + 2);
 
         // We need to use call() so we can set the "this" value inside
         // the handler (otherwise it will be undefined in the handler)
-        handler.call(this, operandA, operandB);
+        const newPC = handler.call(this, operandA, operandB);
 
         // Increment the PC register to go to the next instruction
-        // !!! IMPLEMENT ME
-        const num = (this.reg.IR >> 6) & 0b00000011;
-        this.reg.PC += (num + 1);
+        const count = (this.reg.IR >> 6) & 0b00000011;
+        this.reg.PC += (count + 1);
+
     }
 
     // INSTRUCTION HANDLER CODE:
 
-    /**
-     * HLT
-     */
+    
     HLT() {
         this.stopClock();
     }
-
-    /**
-     * LDI R,I
-     */
+    
     LDI(reg, value) {
-        // !!! IMPLEMENT ME
         this.reg[reg] = value;
     }
-
-    /**
-     * MUL R,R
-     */
-    MUL(regA, regB) {
-        // !!! IMPLEMENT ME
-        // Call the ALU
-        this.alu('MUL', regA, regB);
-    }
-
-    /**
-     * ADD R,R
-     */
-    ADD(regA, regB) {
-        // !!! IMPLEMENT ME
-        // Call the ALU
-        this.alu('ADD', regA, regB);
-    }
-
-    /**
-     * PRN R
-     */
+    
     PRN(regA) {
         // !!! IMPLEMENT ME
         const value = this.reg[regA];
         // Print
         console.log(value);
     }
+    
+    ADD(regA, regB) {
+        this.alu('ADD', regA, regB);
+    }
+    
+    MUL(regA, regB) {
+        this.alu('MUL', regA, regB);
+    }
+    
 }
 
 module.exports = CPU;
